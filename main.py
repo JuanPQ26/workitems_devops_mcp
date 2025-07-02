65244d1650090d625a1f06b6e839a2a02ad493d8
Juan Pablo Quintero <juanpq2003@outlook.com>
2025-07-02 01:09:35 -0500

    agrega un mensaje de bienvenida al inicio del programa.

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
 import asyncio
 
-from services import workitems
-from utils.formatters import format_workitem_type
-
-workitem_type_name = "Task"
-
 
 async def main():
-    workitem_type = await workitems.get_workitem_type_by_name(workitem_type_name)
-    result = format_workitem_type(workitem_type)
-
-    print(result)
-
-    return result
+    print("Welcome to the workitems devops mcp")
 
 
 if __name__ == "__main__":
